ff8e164ef2332e88df34aebd8ced9e487db44bb3
Jerud-Moyer <jerudmoyer@gmail.com>
2020-07-07 13:43:38 -0700

    basic skeleton

---
+++ app.js
@@ -0,0 +1,11 @@
+// import functions and grab DOM elements
+
+const playButton = document.querySelector('#but-ton');
+const remainingTries = document.querySelector('#remaining-tries');
+const hiLow = document.querySelector('#hi-low');
+const winLose = document.querySelector('#win-lose');
+
+
+// initialize state
+
+// set event listeners to update state and DOM
